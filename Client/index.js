b3b64a76e78429b92b2fea4388be3169a80cd005
antonii1357 <antonii.spam@gmail.com>
2024-08-07 12:52:49 -0600

    Removed unused imports, added reconnect function, and aligned table td's to the center

--- Client/index.js
+++ Client/index.js
@@ -1,32 +1,41 @@
 const WebSocket = require('ws')
 const {exec} = require('child_process')
-const si = require('systeminformation')
-const os = require('os')
 const screenshot = require('screenshot-desktop')
 const fs = require('fs')
+let ws
 
-const ws = new WebSocket('ws://localhost:3000')
+function connect() {
+	ws = new WebSocket('ws://localhost:3000')
 
-ws.on('open', () => {
-	console.log('Connected to server')
-})
+	ws.on('open', () => console.log('Connected to server'))
 
-ws.on('message', async (message) => {
-	const data = JSON.parse(message)
-	const requestId = data.requestId
-	if (data.type === 'command') {
-		exec(data.data, (error, stdout, stderr) => {
-			ws.send(JSON.stringify({type: 'result', stdout, stderr, requestId}))
-		})
-	} else if (data.type === 'screenshot') {
-		await screenshot({ filename: 'ss.jpg' })
-		const buffer = fs.readFileSync('./ss.jpg', { encoding: 'base64' })
-		ws.send(JSON.stringify({ type: 'screenshot', buffer, requestId}))
-	} else if (data.type === 'metrics') {
-		const metrics = await getMetrics()
-		ws.send(JSON.stringify({type: 'metrics', metrics, requestId}))
-	}
-})
+	ws.on('close', () => {
+		console.log('Disconnected from server. Attempting to reconnect...')
+		setTimeout(connect, 5000)
+	})
+
+	ws.on('error', (error) => {
+		console.error('WebSocket error:', error)
+		ws.close()
+	})
+
+	ws.on('message', async (message) => {
+		const data = JSON.parse(message)
+		const requestId = data.requestId
+		if (data.type === 'command') exec(data.data, (error, stdout, stderr) => ws.send(JSON.stringify({type: 'result', stdout, stderr, requestId})))
+		else if (data.type === 'screenshot') {
+			await screenshot({ filename: 'ss.jpg' })
+			const buffer = fs.readFileSync('./ss.jpg', { encoding: 'base64' })
+			ws.send(JSON.stringify({ type: 'screenshot', buffer, requestId}))
+		} else if (data.type === 'metrics') {
+			const metrics = await getMetrics()
+			ws.send(JSON.stringify({type: 'metrics', metrics, requestId}))
+		}
+	})
+}
+
+connect()
+
 function getMetrics() {
 	return new Promise((resolve, reject) => {
 		exec('metrics.exe', (error, stdout, stderr) => {
